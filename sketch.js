0cfdcfcadeb870844d746d680cb2c45df58d3af5
vyasshivansh25 <118053495+vyasshivansh25@users.noreply.github.com>
2023-01-07 03:31:56 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var btn2;
+var angle=60;
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+ 
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+   btn2 = createImg('up.png');
+  btn2.position(20,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+
+  ground = Bodies.rectangle(100,400,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+
+
+ // ellipse(ball.position.x,ball.position.y,20);
+  
+ 
+console.log(ground.position.y);
+
+ // Matter.Body.rotate(ground,angle);
+  push();
+  translate(ground.position.x,ground.position.y);
+ rotate(angle);
+  rect(0,0,500,20);
+  pop();
+  angle+=0.1;
+  
+}
+
+function vForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
+  
